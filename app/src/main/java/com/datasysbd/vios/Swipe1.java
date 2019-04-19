1e20bfe693f4872b3b9255a7c171fc6cadb881fd
Samimsam94 <samimsam9489@gmail.com>
2019-04-20 03:42:52 +0600

    UI updated Splash screen Login

--- app/src/main/java/com/datasysbd/vios/Swipe1.java
+++ app/src/main/java/com/datasysbd/vios/Swipe1.java
@@ -1,7 +1,5 @@
 package com.datasysbd.vios;
 
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,7 +10,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_swipe1);
 
     }
 }
