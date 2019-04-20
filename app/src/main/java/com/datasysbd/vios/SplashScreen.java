733760f099224a98e2b9ddf57d8caeac58918820
Samimsam94 <samimsam9489@gmail.com>
2019-04-20 11:31:34 +0600

    Splash Screen Swipte 2

--- app/src/main/java/com/datasysbd/vios/SplashScreen.java
+++ app/src/main/java/com/datasysbd/vios/SplashScreen.java
@@ -5,20 +5,21 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash_screen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
-    private static int splashTimeout=4000;
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(Splash_screen.this, Swipe1.class);
+                Intent intent=new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },splashTimeout);
+        },4000);
     }
 }
